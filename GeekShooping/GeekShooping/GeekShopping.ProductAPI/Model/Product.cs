d6bf471b7ca942a4f9311b7838352e417a72211f
giovanijm <gmariano@tecnologia-unica.com.br>
2024-09-06 19:32:31 -0300

    COnfiguração de acesso ao DB e as migrations

---
+++ GeekShooping/GeekShooping/GeekShopping.ProductAPI/Model/Product.cs
@@ -0,0 +1,46 @@
+﻿using GeekShopping.ProductAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model;
+
+[Table("product")]
+public class Product : BaseEntity
+{
+    /// <summary>
+    /// Nome do produto
+    /// </summary>
+    [Column("name")]
+    [Required]
+    [StringLength(150)]
+    public string Name { get; set; } = null !;
+
+    /// <summary>
+    /// Preço do produto
+    /// </summary>
+    [Column("price")]
+    [Required]
+    [Range(1, 10000)]
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// Descrição do produto
+    /// </summary>
+    [Column("description")]
+    [StringLength(500)]
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Categoria do produto
+    /// </summary>
+    [Column("category_name")]
+    [StringLength(50)]
+    public string? CategoryName { get; set; }
+
+    /// <summary>
+    /// Url da imagem do produto
+    /// </summary>
+    [Column("image_url")]
+    [StringLength(300)]
+    public string? ImageUrl { get; set; }
+}
---
+++ GeekShooping/GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,42 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 2))));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { 
+        Title = "GeekShopping.ProductAPI", 
+        Version = "v1",
+        Description = "API de estudo sobre microserviços em .net 6, conhecimento sendo adquirido nas aulas da Udemy.",
+        Contact = new OpenApiContact { 
+            Email = "giovanijm@gmail.com",
+            Name = "Giovani Júlio Mariano",
+            Url = new Uri("https://sistemallos.com.br"),
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
